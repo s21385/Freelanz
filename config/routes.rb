94b5fdaedd5844a0d56f7ab4e124b06290b68ee2
Francis Poitras <s21385@hotmail.com>
2018-05-28 18:43:30 -0400

    francis27May18h43

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+resources :projects, only: [:index, :show]
+resources :groups, only: [:index]
+
+resources :users do
+  resources :projects
+  resources :groups
+end
+
+
 end
