1b02d82ee409e16c9241ce36999fceb1756c64c1
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-05-30 13:54:06 -0400

    Updated Positions Controller

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,32 +1,36 @@
 class PositionsController < ApplicationController
-  dsdfsd dependent: :destroy
+  before_action :set_position, only: [ :new, :create, :destroy ]
 
   def new
+    # @project = Project.find(params[:project_id])
     @position = Position.new
-  end
-
-  def show
-    @position = Position.find(params[:project_id])
   end
 
   def edit
   end
 
   def create
-    @project.user = current_user
+    # @project = Project.find(params[:project_id])
     @position = Position.new(position_params)
-    @project.user = current_user
+
+    @position.project = @project
+
+
+    @position.save
+    redirect_to position_path(@position)
   end
 
   def update
     @position = Position.find(params[:id])
     @position.update(position_params)
+
     redirect_to position_path(@position)
   end
 
   def destroy
-      @position.destroy
-      redirect_to projects_path
+    @project = @position.project
+    @position.destroy
+    redirect_to project_path(@project)
   end
 
   private
@@ -36,6 +40,6 @@ class PositionsController < ApplicationController
   end
 
   def set_position
-    @position = Position.find(:project_id)
+    @position = Position.find(:position_id)
   end
 end
