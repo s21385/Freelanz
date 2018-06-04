fbec591494542ef204110a3f6d66b7b60a4b70c7
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-06-04 11:56:11 -0400

    Cloudinary & Carrierwave Complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_004505) do
+ActiveRecord::Schema.define(version: 2018_06_04_152249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2018_06_04_004505) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.string "status"
+    t.string "photo"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
