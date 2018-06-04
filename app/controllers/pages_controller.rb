887ef9272e5660355c922a90cab5be1680cfc2ba
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-06-04 09:59:22 -0400

    On To the Second Row of Cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  # skip_before_action :authenticate_user!, only: :home
 
   def home
-    @projects = Project.all
+    @projects = current_user.projects
+    @user_positions = current_user.user_positions if current_user.present?
   end
 
   # def position
