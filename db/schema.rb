9e4b174f63a159ffe8e1d5c5f10ac1192f0ee931
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-06-04 09:16:13 -0400

    Cards Finally Working

--- db/schema.rb
+++ db/schema.rb
@@ -76,11 +76,7 @@ ActiveRecord::Schema.define(version: 2018_06_01_182153) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
-<<<<<<< HEAD
-    t.string "status"
-=======
     t.string "status", default: "Started"
->>>>>>> master
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
