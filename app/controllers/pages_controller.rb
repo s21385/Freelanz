77c6ccace760427e8774429952b6f31f643373b5
Francis Poitras <s21385@hotmail.com>
2018-06-04 18:21:51 -0400

    upgrades to sign up including LinkedIN

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 
 class PagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!
 
   def home
     @projects = current_user.projects
