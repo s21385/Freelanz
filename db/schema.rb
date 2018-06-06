32f74cd02df1acd1899e9a652285c8235909c933
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-06-06 11:44:11 -0400

    Cards Showing Properly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_05_180712) do
+ActiveRecord::Schema.define(version: 2018_06_06_014944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,7 +78,7 @@ ActiveRecord::Schema.define(version: 2018_06_05_180712) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
-    t.string "status", default: "Started"
+    t.string "status"
     t.string "photo"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
@@ -126,6 +126,7 @@ ActiveRecord::Schema.define(version: 2018_06_05_180712) do
     t.string "linkedin_id"
     t.string "github_id"
     t.string "skill_level"
+    t.text "linkedin_photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
