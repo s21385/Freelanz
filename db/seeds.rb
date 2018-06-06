c614314147c3cb8ba594d4e0240753f2b07e55d8
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-06-06 15:29:57 -0400

    Position Edit Works

--- db/seeds.rb
+++ db/seeds.rb
@@ -145,7 +145,7 @@ urls = ["adele.jpg", "buscemi.jpg", "deniro.jpg", "ergogan.jpg", "hoffman.jpg",
 10.times do
   user = User.create!(
     email: Faker::Internet.email,
-    password: "password123",
+    password: "123456",
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     photo: get_path1(urls.sample),
