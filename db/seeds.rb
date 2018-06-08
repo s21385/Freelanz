e517b785e8cf7f01f87f36cb679a84f85d79684c
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-06-08 12:59:46 -0400

    Fixed Final Details

--- db/seeds.rb
+++ db/seeds.rb
@@ -417,7 +417,7 @@ user_position = UserPosition.create!(
   user: user_1,
   position: position_1,
   status: "pending",
-  rate_cents: 123
+  rate_cents: 180
 )
 
 user_2 = User.create!(
@@ -441,7 +441,7 @@ user_position = UserPosition.create!(
 position_2 = Position.create!(
   project: alexs_project,
   name: "Database Admin",
-  rate_cents: 234234,
+  rate_cents: 170,
   status: "Open",
   skill_level: "Junior Programmer"
 )
